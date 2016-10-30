62cf310f7a11b8b6168c7c6bf83bc5d9c943844d
jerry-topshark <ysong719@qq.com>
2016-10-30 23:58:53 +0800

    add router and controllers

---
+++ controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: developer
+ * Date: 16/10/30
+ * Time: 22:56
+ */
+
+namespace controllers;
+
+
+class HomeController
+{
+
+}
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: developer
+ * Date: 16/10/30
+ * Time: 00:12
+ */
